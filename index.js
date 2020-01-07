29fc100f617fe247bc4b212f35b6413d2f43ae6a
于新哲 <>
2020-01-07 16:29:08 +0800

    update config

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 module.exports = {
-  extends: [
-    'airbnb',
-  ],
+  extends: ['airbnb'],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 7,
     ecmaFeatures: {
-      legacyDecorators: true,
-    },
+      legacyDecorators: true
+    }
   },
   globals: {
     window: false,
-    document: false,
+    document: false
   },
   rules: {
     'max-len': [0],
     'import/no-unresolved': [0],
-    'no-console': [2, { "allow": ["warn", "error"] }],
+    'no-console': [2, { allow: ['warn', 'error'] }],
     'no-param-reassign': [0],
     'arrow-parens': [2, 'as-needed'],
-    'prefer-destructuring': ["error", { "object": true, "array": false }],
-    'react/jsx-filename-extension': [0],
+    'prefer-destructuring': [2, { object: true, array: false }],
+    'react/jsx-filename-extension': [0]
   }
-}
+};
